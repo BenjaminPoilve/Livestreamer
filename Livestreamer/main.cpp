165e8769513d4d8e83b74855e556600ad009539d
Benjamin Poilve <benjamin.poilve@student.ecp.fr>
2015-09-25 16:06:14 +0200

    Initial Commit

---
+++ Livestreamer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Livestreamer
+//
+//  Created by Ben on 25/09/2015.
+//  Copyright (c) 2015 -. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
